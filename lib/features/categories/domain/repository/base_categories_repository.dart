4d6f81c81b8029c1365c949b09d307e243d50cf0
AghyadHamdoun <100805550+AghyadHamdoun@users.noreply.github.com>
2023-04-06 22:25:13 +0300

    domain layer done!

--- lib/features/categories/domain/repository/base_categories_repository.dart
+++ lib/features/categories/domain/repository/base_categories_repository.dart
@@ -1,3 +1,4 @@
+import 'package:caphore/features/categories/domain/usecases/get_gategory_products_usecase.dart';
 import 'package:caphore/features/categories/domain/usecases/get_product_details_usecase.dart';
 import 'package:dartz/dartz.dart';
 
@@ -6,7 +7,7 @@ import 'package:caphore/features/categories/domain/entities/products.dart';
 import 'package:flutter/foundation.dart';
 
 abstract class BaseCategoriesRepository {
-  Future<Either<Failure, List<Product>>> getCategoryProducts();
+  Future<Either<Failure, List<Product>>> getCategoryProducts(CategoryProductsParameters parameters);
 
   Future<Either<Failure, Product>> getProductDetails(ProductDetailsParameters parameters);
 
