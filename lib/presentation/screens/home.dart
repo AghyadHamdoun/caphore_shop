ba8d80ac81f6b8f73049b6671def0a9b90221c32
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-04-06 13:50:41 +0300

    home init done

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,122 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../core/utils/app_color.dart';
+import '../widgets/BrandCard.dart';
+import '../widgets/CategoryNameAndShowAll.dart';
+import '../widgets/productcard.dart';
 import '../widgets/salesavatar.dart';
 import '../widgets/textformfild.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> imeges = [
+      "https://images.unsplash.com/photo-1564419320461-6870880221ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      "https://plus.unsplash.com/premium_photo-1673971700988-346588461fa7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+      "https://images.unsplash.com/photo-1680263547745-4e0555920ea2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    ];
+
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: maintextform(),
-          ),
-          SizedBox(
-            height: 144.h,
-            width: 100.w,
-            child: Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 20,
-                itemBuilder: (BuildContext context, int index) {
-                  return const salesavatar();
-                },
-              ),
-            ),
-          ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 30.h,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("new products"),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "show all >",
-                      style: TextStyle(fontSize: 16.sp, color: Colors.blue),
+            padding:
+                EdgeInsets.only(top: 30.h, left: 5.w, right: 5.w, bottom: 0),
+            child: const maintextform(),
+          ),
+          Expanded(
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              children: [
+                SizedBox(
+                  height: 150.h,
+                  child: CarouselSlider(
+                    items: imeges
+                        .map(
+                          (item) => Padding(
+                            padding: EdgeInsets.only(bottom: 10.h),
+                            child: Container(
+                              height: 150.h,
+                              width: 300.w,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(item),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(20.r),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 2,
+                        enlargeCenterPage: true),
+                  ),
+                ),
+                SizedBox(
+                  height: 144.h,
+                  width: double.infinity,
+                  child: Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 20,
+                      itemBuilder: (BuildContext context, int index) {
+                        return const salesavatar();
+                      },
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 190.h,
-              width: double.infinity.w,
-              child: Expanded(
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 20,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                Container(
-                                  height: 100.h,
-                                  width: 80.w,
-                                  color: Colors.blue,
-                                ),
-                                Container(
-                                  height: 25.h,
-                                  width: 80.w,
-                                  decoration: BoxDecoration(
-                                      color: Colors.red,
-                                      borderRadius:
-                                          BorderRadius.circular(20.r)),
-                                ),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SizedBox(
-                                child: Text(
-                                  "productname",
-                                  style: TextStyle(fontSize: 16.sp),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 30.h,
-                              width: 80.w,
-                              child: Expanded(
-                                child: ListTile(
-                                  title: Text(
-                                    "price",
-                                    style: TextStyle(
-                                        fontSize: 16.sp, color: Colors.blue),
-                                  ),
-                                  subtitle: Text(
-                                    "orginal price",
-                                    style: TextStyle(
-                                        decoration: TextDecoration.lineThrough,
-                                        fontSize: 12.sp,
-                                        color: Colors.grey),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
                 ),
-              ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CategoryNameAndShowAll(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 200.h,
+                    width: double.infinity.w,
+                    child: Expanded(
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 20,
+                        itemBuilder: (BuildContext context, int index) {
+                          return const productcard();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: brandcard(),
+                ),
+              ],
             ),
           ),
         ],
---
+++ lib/presentation/widgets/CategoryNameAndShowAll.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CategoryNameAndShowAll extends StatelessWidget {
+  const CategoryNameAndShowAll({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30.h,
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("new products"),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "show all >",
+              style: TextStyle(fontSize: 16.sp, color: Colors.blue),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
