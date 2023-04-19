a3ba1ef7f2195432810b63d6b47fb6b5f5701034
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-04-19 10:20:15 +0300

    bottom navigation done from laith!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:caphore/features/categories/presentation/screeens/Categorys.dart';
+import 'package:caphore/features/categories/presentation/screeens/SubCategorys.dart';
+import 'package:caphore/features/categories/presentation/screeens/categoryproducts.dart';
 import 'package:caphore/features/categories/presentation/screeens/home.dart';
+import 'package:caphore/features/categories/presentation/screeens/product.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,6 +14,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    int currentPage = 0;
+    List pages = [
+      const Home(),
+      const Categorys(),
+    ];
     return ScreenUtilInit(
       designSize: const Size(393, 786),
       minTextAdapt: true,
@@ -20,9 +29,39 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: const Home(),
+          home: Scaffold(
+            body: pages[currentPage],
+            bottomNavigationBar: BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.category),
+                  label: "categories",
+                ),
+              ],
+              currentIndex: currentPage,
+              onTap: (index){
+                currentPage=index;
+              },
+              selectedFontSize: 16.sp,
+              selectedIconTheme: IconThemeData(size: 30.r),
+              unselectedFontSize: 12.sp,
+              unselectedIconTheme: IconThemeData(size: 23.r),
+            ),
+          ),
+          routes: {
+            "/Home": (context) => const Home(),
+            "/Categories": (context) => const Categorys(),
+            "/CategoryProducts": (context) => const categoryproducts(),
+            "product": (context) => const product(),
+            "/subcategories": (context) => const subcategorys(),
+          },
         );
       },
     );
   }
 }
+
