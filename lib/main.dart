6282b28c0d29ff19e6bcf7da3dcc77ea90e2df09
AghyadHamdoun <100805550+AghyadHamdoun@users.noreply.github.com>
2023-04-23 19:03:24 +0300

    localization from aghyad

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,40 @@
 import 'package:caphore/core/services/services_locator.dart';
 import 'package:caphore/core/utils/routes.dart';
 import 'package:caphore/features/categories/presentation/screeens/pages/pages.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   ServicesLocator().init();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+      EasyLocalization(
+      supportedLocales: const [Locale('ar', 'SA'), Locale('ar', 'SA')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('ar', 'SA'),
+      child: const MyApp()
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(393, 786),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
           debugShowCheckedModeBanner: false,
-          home:const MyPages(),
+          home: const MyPages(),
           routes: MyRoutes.routes,
         );
       },
