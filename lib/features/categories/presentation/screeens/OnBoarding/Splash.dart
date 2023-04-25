c0aee1df80c5ff8d0aa5bfe773b0bbe08e847d69
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-04-25 22:10:10 +0300

    OB init for widgets !

---
+++ lib/features/categories/presentation/screeens/OnBoarding/Splash.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          children:
+          [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
