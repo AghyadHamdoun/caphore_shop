5e2d56b28b46add26e2de13ff873a36964c48a8c
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-05-06 13:06:50 +0300

    fix !

--- lib/features/categories/presentation/screeens/categoryproducts.dart
+++ lib/features/categories/presentation/screeens/categoryproducts.dart
@@ -7,50 +7,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
 class CategoryProducts extends StatelessWidget {
   final int categoryId;
   final String categoryName;
 
-  
-   const CategoryProducts({super.key, required this.categoryId, required this.categoryName});
+  const CategoryProducts(
+      {super.key, required this.categoryId, required this.categoryName});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-  create: (context) => sl<CategoriesBloc>(),
-  child: BlocBuilder<CategoriesBloc, CategoriesState>(
-  builder: (context, state) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:
-                  EdgeInsets.only(top: 30.h, left: 0, right: 0, bottom: 10.h),
-              child: const maintextform(),
-            ),
-             Text(
-                categoryName),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, childAspectRatio: 0.8.h),
-                itemCount: 20,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return ProductCard(productname: '', price: '', orginalprice: '', image: '',);
-                },
+      create: (context) => sl<CategoriesBloc>(),
+      child: BlocBuilder<CategoriesBloc, CategoriesState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 30.h, left: 0, right: 0, bottom: 10.h),
+                    child: const maintextform(),
+                  ),
+                  Text(categoryName),
+                  Expanded(
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, childAspectRatio: 0.6.h),
+                      itemCount: 20,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ProductCard(
+                          productname: '',
+                          price: '',
+                          orginalprice: '',
+                          image: '',
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
-  },
-),
-);
   }
 }
