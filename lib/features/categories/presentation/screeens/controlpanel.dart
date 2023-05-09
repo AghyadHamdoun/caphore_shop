140e511aa56849e552aabd8fc1e6c49b5e0f8ab6
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-05-09 22:00:20 +0300

    LEGAL PAGE

---
+++ lib/features/categories/presentation/screeens/controlpanel.dart
@@ -0,0 +1,38 @@
+import 'package:caphore/features/categories/presentation/screeens/widgets/LegalPageWidget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/utils/app_color.dart';
+
+class ControlPanel extends StatelessWidget {
+  const ControlPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            children: const [
+              LegalPageWidget(
+                  name: "الشروط والأحكام",
+                  svgpath: "assets/images/justice-law-svgrepo-com.svg"),
+              LegalPageWidget(
+                  name: "لمحة عن كافور",
+                  svgpath: "assets/images/about-svgrepo-com.svg"),
+              LegalPageWidget(
+                  name: "سياسة الخصوصية",
+                  svgpath: "assets/images/shield-antivirus-svgrepo-com.svg"),
+              LegalPageWidget(
+                  name: " تواصل معنا",
+                  svgpath:
+                      "assets/images/headphones-microphone-svgrepo-com.svg"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
