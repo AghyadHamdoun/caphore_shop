70c2b8bbacd27a8d70f957ec2fd32c7b50ffe5da
LaithMojjadamy <mohamadallays.mg@gmail.com>
2023-10-12 20:15:57 +0300

    تعديلات 7

--- lib/features/categories/presentation/screeens/pages/pages.dart
+++ lib/features/categories/presentation/screeens/pages/pages.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:caphore/core/services/services_locator.dart';
 import 'package:caphore/core/utils/app_color.dart';
 import 'package:caphore/features/attributes/presentation/controller/attributes_bloc.dart';
@@ -11,10 +12,11 @@ import 'package:caphore/features/categories/presentation/screeens/pages/controlp
 import 'package:caphore/features/categories/presentation/screeens/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class MyPages extends StatefulWidget {
   const MyPages({Key? key}) : super(key: key);
@@ -26,143 +28,171 @@ class MyPages extends StatefulWidget {
 class _MyPagesState extends State<MyPages> with AutomaticKeepAliveClientMixin {
   late int select = 1;
   late PageController controller;
+  bool hasnet = true;
 
   @override
   void initState() {
     super.initState();
+    connectivity();
     controller = PageController(initialPage: select);
+  }
+
+  void connectivity() async {
+    var net = await Connectivity().checkConnectivity();
+    //هاد فحص النت كلو تمام بس عروض الديالوغ  بس حطو بمحلو نسيت شلون
+    if (net == ConnectivityResult.mobile || net == ConnectivityResult.wifi) {
+      hasnet = true;
+      print(net); //go
+    } else {
+      hasnet = false;
+      print(net);
+      //جرب هيك طفي النت وشغلو قبل ما تشيسل التعليق
+      //    setState(() {
+      //   AwesomeDialog(
+      //     context: context,
+      //     dialogType: DialogType.error,
+      //     animType: AnimType.topSlide,
+      //     headerAnimationLoop: false,
+      //     title: 'خطأ',
+      //     desc: "يرجى الاتصال بالانترنت",
+      //     btnOkOnPress: () {},
+      //     btnOkIcon: Icons.cancel,
+      //     btnOkColor: Colors.red,
+      //   ).show();
+      // });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
+
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => sl<CategoriesBloc>()
-            ..add(const GetAllCategoriesEvent(page: 1))
-            ..add(const GetMenClothingProductsEvent(
-                pageNum: 1, categoryId: 44, perPage: 10))
-            ..add(const GetWomenClothingProductsEvent(
-                pageNum: 1, categoryId: 42, perPage: 10))
-            ..add(const GetChildrenClothingProductsEvent(
-                pageNum: 1, categoryId: 61, perPage: 10))
-            ..add(const GetFoodProductsEvent(
-                pageNum: 1, categoryId: 195, perPage: 10))
-            ..add(const GetShoesAndBagsProductsEvent(
-                pageNum: 1, categoryId: 102, perPage: 10))
-            ..add(const GetWatchesAndAccessoriesProductsEvent(
-                pageNum: 1, categoryId: 118, perPage: 10))
-            ..add(const GetMobilesProductsEvent(
-                pageNum: 1, categoryId: 123, perPage: 10))
-            ..add(const GetPerfumesProductsEvent(
-                pageNum: 1, categoryId: 108, perPage: 10))
-            ..add(const GetMakeUpProductsEvent(
-                pageNum: 1, categoryId: 112, perPage: 10))
-            ..add(const GetPetsProductsEvent(
-                pageNum: 1, categoryId: 421, perPage: 10))
-            ..add(const GetOffersProductsEvent(
-                pageNum: 1, categoryId: 644, perPage: 10)),
-        ),
-        BlocProvider(
-            create: (context) => sl<AttributesBloc>()
-              ..add(const GetBannersTermsEvent(
-                  pageNum: 1, attributeId: 34, perPage: 100)))
-      ],
-      child: SafeArea(
-        child: WillPopScope(
-          onWillPop: () async {
-            if (select != 1) {
-              controller.jumpToPage(1);
-              setState(() {
-                select = 1;
-              });
-              return false;
-            } else {
-              SystemNavigator.pop();
-              return true;
-            }
-          },
-          child: Scaffold(
-            body: PageView(
-              controller: controller,
-              physics: const NeverScrollableScrollPhysics(),
-              children:  [
-                Brands(),
-              const  Home(),
-                Stores(),
-                Restaurants(),
-               const ControlPanel(),
-              ],
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: [
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    'assets/images/diamond-svgrepo-com.svg',
-                    color: AppColor.accentColor,
-                    height: 25.h,
-                    width: 20.w,
-                  ),
-                  label: "الماركات",
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    'assets/images/home_icon.svg',
-                    color: AppColor.accentColor,
-                    height: 25.h,
-                    width: 20.w,
-                  ),
-                  label: "الرئيسية",
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    "assets/images/store-svgrepo-com.svg",
-                    color: AppColor.accentColor,
-                    height: 25.h,
-                    width: 20.w,
-                  ),
-                  label: "المتاجر",
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    "assets/images/restaurant-plate-svgrepo-com.svg",
-                    color: AppColor.accentColor,
-                    height: 25.h,
-                    width: 20.w,
-                  ),
-                  label: "المطاعم",
-                ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/images/information-button.png',
-                    height: 30.h,
-                    width: 25.w,
-                  ),
-                  label: "معلوماتنا",
-                ),
-              ],
-              currentIndex: select,
-              onTap: (index) {
+        providers: [
+          BlocProvider(
+            create: (context) => sl<CategoriesBloc>()
+              ..add(const GetAllCategoriesEvent(page: 1))
+              ..add(const GetMenClothingProductsEvent(
+                  pageNum: 1, categoryId: 44, perPage: 10))
+              ..add(const GetWomenClothingProductsEvent(
+                  pageNum: 1, categoryId: 42, perPage: 10))
+              ..add(const GetChildrenClothingProductsEvent(
+                  pageNum: 1, categoryId: 61, perPage: 10))
+              ..add(const GetFoodProductsEvent(
+                  pageNum: 1, categoryId: 195, perPage: 10))
+              ..add(const GetShoesAndBagsProductsEvent(
+                  pageNum: 1, categoryId: 102, perPage: 10))
+              ..add(const GetWatchesAndAccessoriesProductsEvent(
+                  pageNum: 1, categoryId: 118, perPage: 10))
+              ..add(const GetMobilesProductsEvent(
+                  pageNum: 1, categoryId: 123, perPage: 10))
+              ..add(const GetPerfumesProductsEvent(
+                  pageNum: 1, categoryId: 108, perPage: 10))
+              ..add(const GetMakeUpProductsEvent(
+                  pageNum: 1, categoryId: 112, perPage: 10))
+              ..add(const GetPetsProductsEvent(
+                  pageNum: 1, categoryId: 421, perPage: 10))
+              ..add(const GetOffersProductsEvent(
+                  pageNum: 1, categoryId: 644, perPage: 10)),
+          ),
+          BlocProvider(
+              create: (context) => sl<AttributesBloc>()
+                ..add(const GetBannersTermsEvent(
+                    pageNum: 1, attributeId: 34, perPage: 100)))
+        ],
+        child: SafeArea(
+          child: WillPopScope(
+            onWillPop: () async {
+              if (select != 1) {
+                controller.jumpToPage(1);
                 setState(() {
-                  select = index;
+                  select = 1;
                 });
-                controller.jumpToPage(select);
-              },
-              selectedItemColor: AppColor.accentColor,
-              selectedFontSize: 16.sp,
-              selectedIconTheme:
-                  IconThemeData(size: 30.r, color: AppColor.accentColor),
-              showUnselectedLabels: true,
-              unselectedIconTheme:
-                  IconThemeData(size: 30.r, color: AppColor.accentColor),
-              unselectedItemColor: AppColor.accentColor,
-              type: BottomNavigationBarType.fixed,
+                return false;
+              } else {
+                SystemNavigator.pop();
+                return true;
+              }
+            },
+            child: Scaffold(
+              body: PageView(
+                controller: controller,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  Brands(),
+                  const Home(),
+                  Stores(),
+                  Restaurants(),
+                  const ControlPanel(),
+                ],
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                items: [
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'assets/images/diamond-svgrepo-com.svg',
+                      color: AppColor.accentColor,
+                      height: 25.h,
+                      width: 20.w,
+                    ),
+                    label: "الماركات",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'assets/images/home_icon.svg',
+                      color: AppColor.accentColor,
+                      height: 25.h,
+                      width: 20.w,
+                    ),
+                    label: "الرئيسية",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      "assets/images/store-svgrepo-com.svg",
+                      color: AppColor.accentColor,
+                      height: 25.h,
+                      width: 20.w,
+                    ),
+                    label: "المتاجر",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      "assets/images/restaurant-plate-svgrepo-com.svg",
+                      color: AppColor.accentColor,
+                      height: 25.h,
+                      width: 20.w,
+                    ),
+                    label: "المطاعم",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/images/information-button.png',
+                      height: 30.h,
+                      width: 25.w,
+                    ),
+                    label: "معلوماتنا",
+                  ),
+                ],
+                currentIndex: select,
+                onTap: (index) {
+                  setState(() {
+                    select = index;
+                  });
+                  controller.jumpToPage(select);
+                },
+                selectedItemColor: AppColor.accentColor,
+                selectedFontSize: 16.sp,
+                selectedIconTheme:
+                    IconThemeData(size: 30.r, color: AppColor.accentColor),
+                showUnselectedLabels: true,
+                unselectedIconTheme:
+                    IconThemeData(size: 30.r, color: AppColor.accentColor),
+                unselectedItemColor: AppColor.accentColor,
+                type: BottomNavigationBarType.fixed,
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   @override
